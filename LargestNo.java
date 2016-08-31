a1a3c317185db54c3969481a0fc693ee5c2d4aca
Priyanka J R <priyankajadhanramrajan@gmail.com>
2016-08-31 11:45:04 +0530

    Create LargestNo.java

---
+++ LargestNo.java
@@ -0,0 +1,29 @@
+package beginner;
+
+import java.util.Scanner;
+
+public class LargestNo 
+{
+	public static void main(String[] args) 
+	{
+		Scanner sc=new Scanner(System.in);
+		System.out.println("Enter three numbers");
+		int a=sc.nextInt();
+		int b=sc.nextInt();
+		int c=sc.nextInt();
+		if(a>b && a>c)
+		{
+			System.out.println("Largest No"+a);
+		}
+		else if(b>c&&b>a)
+		{
+			System.out.println("Largest No"+b);
+		}
+		else
+		{
+			System.out.println("Largest No"+c);
+		}
+		
+	}
+
+}
