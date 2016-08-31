a96b1e53e6c04230b5d646f9a04be626bb3d7b0c
Priyanka J R <priyankajadhanramrajan@gmail.com>
2016-08-31 12:27:44 +0530

    Create OddInInterval.java

---
+++ OddInInterval.java
@@ -0,0 +1,25 @@
+package beginner;
+
+import java.util.Scanner;
+
+public class OddInInterval 
+{
+	public static void main(String[] args) 
+	{
+		Scanner sc=new Scanner(System.in);
+		System.out.println("Enter Starting and Ending Limit");
+		int p=sc.nextInt();
+		int q=sc.nextInt();
+		int count=0;
+		for(int i=p;i<=q;i++)
+		{
+			if(i%2!=0)
+			{
+				count++;
+			}
+		}
+		System.out.println(count);
+		
+	}
+
+}
